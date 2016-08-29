0ad5e2f735c6067992eeb23c384390d5355e963f
ghost60 <570761003@qq.com>
2016-08-29 23:02:25 +0800

    配置文件

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+let path = require('path');
+let node_modules = path.resolve(__dirname, 'node_modules');
+let pathToReact = path.resolve(node_modules, 'react/dist/react.min.js');
+let ExtractTextPlugin = require('extract-text-webpack-plugin');
+let CommonsChunkPlugin=require('webpack/lib/optimize/CommonsChunkPlugin');
+
+module.exports = {
+    entry: [
+      //'webpack/hot/dev-server',
+      //'webpack-dev-server/client?http://localhost:8080',
+      path.resolve(__dirname, 'public/javascript/main.js')
+    ],
+    output:{
+        path:path.resolve(__dirname,'public/javascript'),
+        filename:'bundle.js'
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    module: {
+    loaders:[
+      {test: /\.jsx?$/,loader: 'babel', exclude: /node_modules/,query: {cacheDirectory: true,presets: ['react', 'es2015']}},
+      {test: /\.css$/, loader: ExtractTextPlugin.extract('style','css')},
+      { test: /\.less$/, loader: 'style!css!less'},
+      // { test: /\.less$/, loader: ExtractTextPlugin.extract('style','css','less')},
+      // { test: /\.scss$/, loader: 'style!css!sass'}
+      { test: /\.scss$/, loader: ExtractTextPlugin.extract('style','css!sass')}
+  	],
+  	noParse: [pathToReact]
+	},
+  babel: {
+        presets: ['es2015', 'stage-0', 'react'],
+        plugins: ["transform-runtime",['antd', { 'style': "css"}]]
+    },
+  plugins: [
+      // new CommonsChunkPlugin('commons.js'),
+      new ExtractTextPlugin('../stylesheets/main.css')
+    ]
+};
